9ca8535a08cf64e8990a7047a03f04e8f4bb6899
Aswin K <aswinvtk2001@gmail.com>
2025-04-17 23:52:06 +0530

    riverpod for slider

--- lib/features/home/presentation/widgets/start_test_section.dart
+++ lib/features/home/presentation/widgets/start_test_section.dart
@@ -1,25 +1,17 @@
-// slide effect of start_test_widget - Manu
-// use carousel slider(already installed) to slide or use any other if available
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
-// import 'package:gap/gap.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:learnify_app/core/theme/colors/app_colors.dart';
+import 'package:learnify_app/features/home/presentation/providers/start_test_provider.dart';
+import 'package:learnify_app/features/home/presentation/widgets/start_test_widget.dart';
 
-import 'start_test_widget.dart';
-
-class StartTestSection extends StatefulWidget {
+class StartTestSection extends ConsumerWidget {
   const StartTestSection({super.key});
 
   @override
-  State<StartTestSection> createState() => _StartTestSectionState();
-}
+  Widget build(BuildContext context, WidgetRef ref) {
+    final currentIndex = ref.watch(startTestIndexProvider);
 
-class _StartTestSectionState extends State<StartTestSection> {
-  // final CarouselController _controller = CarouselController();
-  int _currentIndex = 0;
-
-  @override
-  Widget build(BuildContext context) {
     final List<Widget> cards = const [
       StartTestWidget(),
       StartTestWidget(),
@@ -31,7 +23,6 @@ class _StartTestSectionState extends State<StartTestSection> {
       children: [
         CarouselSlider.builder(
           itemCount: cards.length,
-          carouselController: null,
           itemBuilder: (context, index, realIndex) {
             return Padding(
               padding: const EdgeInsets.symmetric(horizontal: 8.0),
@@ -43,27 +34,24 @@ class _StartTestSectionState extends State<StartTestSection> {
             viewportFraction: 1,
             enlargeCenterPage: true,
             onPageChanged: (index, reason) {
-              setState(() {
-                _currentIndex = index;
-              });
+              ref.read(startTestIndexProvider.notifier).state = index;
             },
           ),
         ),
-        // Gap(4),
         Row(
           mainAxisAlignment: MainAxisAlignment.center,
           children: List.generate(cards.length, (index) {
             return AnimatedContainer(
               duration: const Duration(milliseconds: 300),
-              width: _currentIndex == index ? 8.0 : 6.0,
-              height: _currentIndex == index ? 8.0 : 6.0,
+              width: currentIndex == index ? 8.0 : 6.0,
+              height: currentIndex == index ? 8.0 : 6.0,
               margin: const EdgeInsets.symmetric(horizontal: 4.0),
               decoration: BoxDecoration(
                 shape: BoxShape.circle,
                 color:
-                    _currentIndex == index
+                    currentIndex == index
                         ? AppColors.primary
-                        : Colors.black.withOpacity(0.3),
+                        : Color.fromRGBO(0, 0, 0, 0.3),
               ),
             );
           }),
